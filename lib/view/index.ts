89743a409306f3c8b203d610fee6b709074b607d
JUANMA <juanmafisolis242@gmail.com>
2020-11-08 02:36:43 -0600

    last

--- lib/view/index.ts
+++
@@ -1,6 +0,0 @@
-const bAdd = document.querySelector('#bAdd') as HTMLButtonElement;
-
-bAdd.addEventListener('click',e=>{
-    alert('Todo bien todo correcto');
-    document.querySelector('#display').textContent = "hola mundo";
-});
